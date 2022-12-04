4785b3b50f84ae24a059e77687ae87f1fc85cf8c
cankoyuncu <2110206542@ogrenci.karabuk.edu.tr>
2022-12-04 21:45:06 +0300

    Update tree.c

--- week8_Tree/tree.c
+++ week8_Tree/tree.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 //leaf sayisini dugumlerin toplamini 
-//agactaki herhangi bir dugumun depth 
+
 //agacin yuksekligini
 
 struct node{
-	
+	int data;
+	struct node *left, *right;
 };
 
 typedef struct node BTREE;
@@ -21,3 +22,42 @@ int leafCount(BTREE *node){
 	else 
 		return leafCount(node->left)+leftCount(node->right)
 }
+
+BTREE *new_node(int data){
+	BTREE *node = (BTREE*)malloc(sizeof(BTREE));
+	node->data=data;
+	node->left=node->right=NULL;
+	return node;
+}
+
+BTREE *insert(BTREE *root, int data){
+	if(root!=NULL){
+		if(data<root->data)
+			root->left = insert (root->left,data);
+		else
+			root->right = insert(root->right,data);
+		
+	}else //root yoksa, root==NULL ise sunlari yapariz
+		root=new_node(10);
+	return root;
+}
+
+int sumBTREE(BTREE *root){
+	if(root=NULL)
+		return 0;
+	return (root->data + sumBTREE(root->left)+sumBTREE(root->right));
+	
+}
+
+//agactaki herhangi bir dugumun derinligini hesaplamak
+
+int find_depth()
+
+
+
+
+
+
+
+
+
