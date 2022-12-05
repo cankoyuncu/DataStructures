69bbb252cff577f7248a351985efaa8afb7aefa8
cankoyuncu <2110206542@ogrenci.karabuk.edu.tr>
2022-12-05 12:39:13 +0300

    Update tree-lab-example.c

--- week8_Tree/tree-lab-example.c
+++ week8_Tree/tree-lab-example.c
@@ -3,12 +3,12 @@
 
 struct node{
 	int data;
-	struct node *left, *right;
+	struct node *left,*right;
 };
 
 typedef struct node BTREE;
 
-//leaf sayisini dugumlerin toplamini bulan fonksiyon
+//leaf sayisini bulan fonksiyon
 
 int leafCount(BTREE *node){
 	if(node==NULL)
@@ -59,10 +59,9 @@ int find_depth(BTREE *root, int data){
 		return 1 + find_depth(root->left,data);
 	else 
 		return 1 + find_depth(root->right,data);
-
 }
 
-//agacin yuksekligini bulan fonk
+//agacin yuksekligini bulan fonksiyon
 
 int tree_height(BTREE *root){
 	if(!root)
@@ -89,14 +88,14 @@ int main(){
 	myroot = insert(myroot,1);
 	myroot = insert(myroot,5);
 	
-	printf("Leaf count on tree: %d\n",leafCount(myroot));
-	printf("Sum of data on tree: %d\n",sumBTREE(myroot));
-	printf("Depth of the given node: %d\n", find_depth(myroot,7));
+	printf("Leaf count on tree :%d\n",leafCount(myroot));
+	printf("Sum of data on tree : %d\n",sumBTREE(myroot));
+	printf("Depth of the given node :%d\n",find_depth(myroot,6));
 	int height = tree_height(myroot);
-	printf("Height of tree:%d\n", height);
+	printf("Height of tree :%d\n",height);
 	
 	getch();
-	return 0;	
+	return 0;
 	
 }
 
