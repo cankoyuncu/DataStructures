e898ef50ad67f4ba00167dd3f01c93d2a28dc82d
Can Koyuncu <99793493+cankoyuncu@users.noreply.github.com>
2023-12-05 17:44:39 +0300

    Update 5-numeric.c

--- Numeric_Analysis/5-numeric.c
+++ Numeric_Analysis/5-numeric.c
@@ -4,24 +4,26 @@
 #define  MATRIX_BOYUTU 5
 
 float determinant(float matrix[MATRIX_BOYUTU][MATRIX_BOYUTU], int size){
-    int c;
-    float det=0,s=1;
-    float b[MATRIX_BOYUTU][MATRIX_BOYUTU];
+    int c; //sutunlar uzerinde rekursif icin c 
+    float det=0,s=1; //isaret carpani s
+    float b[MATRIX_BOYUTU][MATRIX_BOYUTU]; //alt matris b 
     int i,j;
-    int m,n;
+    int m,n; //altmatris indisleri icin m ve n 
     if(size == 1){
-        return (matrix[0][0]);
+        return (matrix[0][0]); // burada matrisim 1x1 boyutuna geldiyse matrisim tek elemanlı demektir ve fonksiyon bu degeri dondurur.
     }
 
-    else{
+    else{ //buraya giren her matrise rekursif uygulanir.  
         det=0;
         for(c=0; c<size; c++){
+            //matrisin her sutunu c degiskeni ile yinelenir.
             m=0;
             n=0;
             for(i=0; i<size; i++){
 
                 for(j=0; j<size; j++){
 
+                    //mevcut sutun ve ilk satır haric tutularak alt matrisimiz b olusturulur.
                     b[i][j] = 0;
                     if(i!=0 && j !=c ){
                         b[m][n] = matrix[i][j];
@@ -51,7 +53,8 @@ int main(){
         {-3, -6, -9, -12, 4},
         {0, 0, 1, 1, 1}
     };
-
+    //Determinant daha sonra determinant fonksiyonu kullanılarak hesaplanır 
+    //ve sonuç yazdırılır.
     float result = determinant(matrix, n);
     printf("Matrisin determinanti: %f", result);
 
